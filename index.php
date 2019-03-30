1843ae85b666b0e0886a327000c648b98a4f4e8d
Shubham Mishra <mishrashubham133@gmail.com>
2019-03-30 20:36:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,59 @@
 <?php include 'db.php'?>
 <?php include 'headnav.php'?>
 <div class="container">
-<br>
-<hr/>
+
 <?php include 'form.php'?>
 <hr/>
-<?php //include 'result.php'?>
-</div>
+<?php include 'menu.php'?>
 
-</div>
+
+<?php
+ if(isset($_GET['search']))
+ {
+	
+			$source=$_GET['source'];
+			$dest=$_GET['destination'];
+			echo "<div>Source: $source<br>
+					Destination : $dest</div>";
+		echo "<div class='row'>";
+			
+	$get_route ="select * from routes where stops like '%$source%'
+				and stops like '%$dest%'";
+	$run_route = mysqli_query($con,$get_route);
+					while($row_routes=mysqli_fetch_array($run_route))
+					{
+						$route_id =$row_routes['id'];
+						//echo "$route_id";
+						$stopname=$row_routes['stops'];
+						?>
+						<div class="col-sm-4">
+									<img width="200" height="200" src="https://busimg.cardekho.com/n/649x396/daimler-confident-of-growth-in-indian-bus-market-2.jpg">
+									
+									<p >
+										<p>
+										<?php echo "route ->$stopname "?>
+										</p>
+							<?php
+							$get_route1 ="select * from buses where route='$route_id'";
+							$run_route1 = mysqli_query($con,$get_route1);
+							while($row_bus=mysqli_fetch_array($run_route1))
+							{
+								$bus_no =$row_bus['bus_no'];
+								
+								$depart =$row_bus['dep_time'];
+								
+											
+								echo "<font color='blue'>Bus no.</font>$bus_no 
+								<font color='red'>Departure Time:</font>$depart<br>
+								";
+								
+								}?>
+						</a></p>
+						</div>
+			<?php	}
+ }
+ ?>
+
+   
+  </div>
 </div>
