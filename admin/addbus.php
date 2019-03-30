00fb375754e9f5898a23d194a684645008e338fd
Shubham Mishra <mishrashubham133@gmail.com>
2019-03-30 22:27:33 +0530

    Update addbus.php

--- admin/addbus.php
+++ admin/addbus.php
@@ -1,6 +1,7 @@
 <?php include "config.php"?>
 <?php if (isset($_POST['save'])) {
 	
+	    $error = false;
 		$source = $_POST['source'];
 		$destination = $_POST['destination'];
 		$route=$_POST['route'];
@@ -8,16 +9,60 @@
 		$dep_time=$_POST['dep_time'];
 		$type=$_POST['type'];
 		
-		mysqli_query($con, "INSERT INTO `buses`(`bus_no`, `source`, `destination`, `route`, `dep_time`, `cat`) 
-		             VALUES ('$busno','$source','$destination','$route','$dep_time','$type')");
-		$_SESSION['message'] = "Address saved"; 
+		// basic awb validation
+            $cdquery1="SELECT * FROM routes  ORDER BY id ASC";
+            $cdresult1=mysqli_query($con,$cdquery1) or die ("Query to get data from firsttable failed: ".mysql_error()); 
+			$cdrow1=mysqli_fetch_array($cdresult1);
+		if ($source!=$cdrow1['source'] and $destination!=$cdrow1['destination']) {
+			$error = true;
+			$routeError = "Please enter your Select Proper Route.";
+		}if (empty($dep_time)) {
+			$error = true;
+			$awbError = "Please enter your Departure Time.";
+		}
+		if(!$error ) {
+			$query="INSERT INTO `buses`(`bus_no`, `source`, `destination`, `route`, `dep_time`, `cat`) 
+		             VALUES ('$busno','$source','$destination','$route','$dep_time','$type')";
+		$res=mysqli_query($con,"INSERT INTO `buses`(`bus_no`, `source`, `destination`, `route`, `dep_time`, `cat`) 
+		             VALUES ('$busno','$source','$destination','$route','$dep_time','$type')" );
+	
 		//header('location: index.php');
-		$errTyp = "success";
-		$errMSG = "Successfully Entered";
-		unset($source);
-}
+		if ($res) {
+				$errTyp = "success";
+				$errMSG = "Successfully Entered";  
+					unset($source);
+					unset($destination);
+					unset($route);
+					unset($bus_no);
+					unset($dep_time);
+			} else {
+				$errTyp = "danger";
+				$errMSG = "Something went wrong, try again later...";	
+			}
+}}
 ?>
+<script>
+$( "select[name='source']" ).change(function () {
+    var stateID = $(this).val();
+    if(stateID) {
+        $.ajax({
+            url: "getroute.php",
+            dataType: 'Json',
+            data: {'id':stateID},
+            success: function(data) {
+                $('select[name="route"]').empty();
+                $.each(data, function(key, value) {
+                    $('select[name="route"]').append('<option value="'+ key +'">'+ value +'</option>');
+                });
+            }
+        });
+    }else{
+        $('select[name="route"]').empty();
+    }
+});
+</script>
 <div class="container">
+<h3>Add Bus</h3>
 <hr />
 <?php
 			if ( isset($errMSG) ) {
